53a25d13f619d36323a78dbebde21f57925ec7d4
Mohamed Mousa <mousa1152003@gmail.com>
2024-10-25 23:24:31 +0300

    Count Odd Numbers below n

---
+++ bin/oop/employee.dart
@@ -0,0 +1,29 @@
+/*
+
+Name  text with spaces
+National ID unique value (text) for each employee
+Salary static double value of 1500 pounds
+
+Gender character holds f or m
+
+Available_vacations initially starts with 15 days
+Deduction_per_day static double value of 85.5pounds
+Available_permission_hours initially starts with 20 hours
+Deduction_per_hour static double value of 15.5pounds
+Actual_salary  double value with actual salary after deductions
+ */
+
+import 'dart:ffi';
+
+class Employee{
+  String? name;
+  String? id;
+  static double salary = 1500;
+  Char? gender;
+  int availableVacations =15;
+  static double eduction_per_day = 85.5 ;
+  
+
+
+
+}
