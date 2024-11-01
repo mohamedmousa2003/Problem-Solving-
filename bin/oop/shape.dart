5d5ee6f5c1372eb9c387576676659c1df1cf3a56
Mohamed Mousa <mousa1152003@gmail.com>
2024-11-01 11:01:52 +0200

    create class Shape is a parent class Two child class Rectangular and Triangle extends Shape Every class calculateArea and draw shape

---
+++ bin/oop/shape.dart
@@ -0,0 +1,68 @@
+
+
+import 'dart:io';
+
+class Shape {
+  double width;
+  double height;
+
+  Shape({required this.width, required this.height});
+
+  double calculateArea() {
+    return 0;
+  }
+
+  void draw() {
+    print("invald");
+  }
+}
+
+class Rectangular extends Shape {
+  Rectangular({required super.width, required super.height});
+  @override
+  double calculateArea() {
+    return width*height;
+  }
+
+  @override
+  void draw() {
+    // TODO: implement draw
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        stdout.write("*");
+      }
+      print("");
+    }
+  }
+}
+
+class Triangle extends Shape {
+  Triangle({required super.width, required super.height});
+  @override
+  void draw() {
+    // TODO: implement draw
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j <=i; j++) {
+        stdout.write("*");
+      }
+      print("");
+    }
+  }
+  @override
+  double calculateArea() {
+    // TODO: implement calculateArea
+    return 0.5*width*height;
+  }
+}
+
+void main(){
+  // create object Rectangular
+  Rectangular rectangular=Rectangular(height: 5,width:10 );
+  rectangular.draw();
+  print(rectangular.calculateArea());
+  print("----------------------------------------");
+  Triangle triangle =Triangle(width: 3, height: 4);
+  triangle.draw();
+  print(triangle.calculateArea());
+
+}
