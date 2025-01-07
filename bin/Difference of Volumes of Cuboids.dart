9b372dc37ce0ada633cc910f5f37f5389d35594d
Mohamed Mousa <mousa1152003@gmail.com>
2025-01-07 21:25:01 +0200

    Difference of Volumes of Cuboids

---
+++ bin/Difference of Volumes of Cuboids.dart
@@ -0,0 +1,15 @@
+/*
+link problem = https://www.codewars.com/kata/58cb43f4256836ed95000f97/train/dart
+ */
+
+int findDifference(List<int> a, List<int> b) {
+  int x = a.reduce((value, element) => value*element);
+  int y = b.reduce((value, element) => value*element);
+  return (x - y).abs() ;
+}
+
+void main(){
+ List<int> a =[9, 7, 2];
+ List<int> b =[5, 2, 2];
+ print(findDifference(a, b));
+}
